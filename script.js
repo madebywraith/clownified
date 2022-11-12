8422f53cc0f7dd67582d61ecf93d2fc23e43f0bc
Wraith <wraith@wraithfx.com>
2022-11-12 03:04:24 +0000

    init commit

---
+++ script.js
@@ -0,0 +1,165 @@
+const clown = `
+    <g transform="matrix(1,0,0,1,-11.0906,-11.2662)">
+        <path d="M13.845,27.933C13.845,27.933 11.798,26.465 13.243,24.809C13.243,24.809 10.964,22.796 12.815,20.905C12.815,20.905 11.577,16.832 14.953,16.596C14.953,16.596 14.563,12.524 19.032,12.998C19.032,12.998 21.534,11.196 23.388,13.247C23.388,13.247 25.921,11.639 27.231,13.649" style="fill:rgb(210,47,39);fill-rule:nonzero;"/>
+        <path d="M58.155,27.933C58.155,27.933 60.202,26.465 58.757,24.809C58.757,24.809 61.036,22.796 59.185,20.905C59.185,20.905 60.423,16.832 57.047,16.596C57.047,16.596 57.437,12.524 52.968,12.998C52.968,12.998 50.466,11.196 48.612,13.247C48.612,13.247 46.079,11.639 44.769,13.649" style="fill:rgb(210,47,39);fill-rule:nonzero;"/>
+        <circle cx="36" cy="36" r="23" style="fill:white;"/>
+        <path d="M49.807,46.546C49.807,46.546 48.227,54.976 35.997,55.306C23.817,54.866 22.187,46.546 22.187,46.546C21.727,44.816 20.947,40.786 24.467,42.736C27.167,44.236 29.847,47.106 35.997,47.286C42.157,47.106 44.827,44.236 47.527,42.736C51.047,40.786 50.277,44.816 49.807,46.546Z" style="fill:rgb(210,47,39);fill-rule:nonzero;"/>
+        <path d="M35.998,52.734C29.936,52.734 26.068,49.425 25.087,47.82C24.105,46.215 25.637,46.664 25.953,46.817C28.267,47.933 29.994,49.515 35.998,49.72M35.998,52.734C42.06,52.734 45.928,49.425 46.909,47.82C47.891,46.215 46.359,46.664 46.043,46.817C43.73,47.933 42.002,49.515 35.998,49.72" style="fill:white;fill-rule:nonzero;"/>
+        <path d="M23.443,21.175C26.342,19.62 29.637,19.562 32.099,21.175" style="fill:none;fill-rule:nonzero;stroke:black;stroke-width:2px;"/>
+        <path d="M39.901,21.175C42.8,19.62 46.095,19.562 48.557,21.175" style="fill:none;fill-rule:nonzero;stroke:black;stroke-width:2px;"/>
+        <path d="M49.808,46.546C49.808,46.546 48.228,54.976 35.998,55.306C23.818,54.866 22.188,46.546 22.188,46.546C21.728,44.816 20.948,40.786 24.468,42.736C27.168,44.236 29.848,47.106 35.998,47.286C42.158,47.106 44.828,44.236 47.528,42.736C51.048,40.786 50.278,44.816 49.808,46.546Z" style="fill:none;fill-rule:nonzero;stroke:black;stroke-width:2px;"/>
+        <path d="M13.845,27.933C13.845,27.933 11.798,26.465 13.243,24.809C13.243,24.809 10.964,22.796 12.815,20.905C12.815,20.905 11.577,16.832 14.953,16.596C14.953,16.596 14.563,12.524 19.032,12.998C19.032,12.998 21.534,11.196 23.388,13.247C23.388,13.247 25.921,11.639 27.231,13.649" style="fill:none;fill-rule:nonzero;stroke:black;stroke-width:2px;"/>
+        <circle cx="36" cy="36" r="23" style="fill:none;stroke:black;stroke-width:2px;"/>
+        <path d="M58.155,27.933C58.155,27.933 60.202,26.465 58.757,24.809C58.757,24.809 61.036,22.796 59.185,20.905C59.185,20.905 60.423,16.832 57.047,16.596C57.047,16.596 57.437,12.524 52.968,12.998C52.968,12.998 50.466,11.196 48.612,13.247C48.612,13.247 46.079,11.639 44.769,13.649" style="fill:none;fill-rule:nonzero;stroke:black;stroke-width:2px;"/>
+        <ellipse cx="27.771" cy="30.818" rx="3.5" ry="6.305" style="fill:none;stroke:black;stroke-width:2px;stroke-linecap:butt;stroke-linejoin:miter;"/>
+        <ellipse cx="44.229" cy="30.822" rx="3.5" ry="6.305" style="fill:none;stroke:black;stroke-width:2px;stroke-linecap:butt;stroke-linejoin:miter;"/>
+        <circle cx="36" cy="38.978" r="2.735"/>
+        <ellipse cx="27.771" cy="30.818" rx="3.5" ry="6.305" style="fill:none;stroke:rgb(146,211,245);stroke-width:2.1px;stroke-linecap:butt;stroke-linejoin:miter;"/>
+        <ellipse cx="44.229" cy="30.822" rx="3.5" ry="6.305" style="fill:none;stroke:rgb(146,211,245);stroke-width:2.1px;stroke-linecap:butt;stroke-linejoin:miter;"/>
+        <path d="M30.268,30.816C30.268,33.856 28.948,36.126 27.768,36.126C26.588,36.126 25.268,33.856 25.268,30.816C25.268,27.786 26.588,25.516 27.768,25.516C28.948,25.516 30.268,27.786 30.268,30.816Z" style="fill:black;"/>
+        <path d="M46.728,30.826C46.728,33.856 45.408,36.126 44.228,36.126C43.048,36.126 41.728,33.856 41.728,30.826C41.728,27.786 43.048,25.516 44.228,25.516C45.408,25.516 46.728,27.786 46.728,30.826Z" style="fill:black;"/>
+        <circle cx="36.056" cy="38.978" r="2.735" style="fill:rgb(210,47,39);"/>
+    </g>
+`
+
+let regularVerifiedPath = 'svg path[d^="M22.25 12c0-1.43-.88-2.67-2.19-3.34.46-1.39.2-2.9-.81-3.91s-2.52-1.27-3.91-.81c-.66-1.31-1.91-2.19-3.34-2.19s-2.67.88-3.33 2.19c-1.4-.46-2.91-.2-3.92.81s-1.26 2.52-.8 3.91c-1.31.67-2.2 1.91-2.2 3.34s.89 2.67 2.2 3.34c-.46 1.39-.21 2.9.8 3.91s2.52 1.26 3.91.81c.67 1.31 1.91 2.19 3.34 2.19s2.68-.88 3.34-2.19c1.39.45 2.9.2 3.91-.81s1.27-2.52.81-3.91c1.31-.67 2.19-1.91 2.19-3.34zm-11.71 4.2L6.8 12.46l1.41-1.42 2.26 2.26 4.8-5.23 1.47 1.36-6.2 6.77z"]'
+
+const regulartick = `
+<g><path d="M22.25 12c0-1.43-.88-2.67-2.19-3.34.46-1.39.2-2.9-.81-3.91s-2.52-1.27-3.91-.81c-.66-1.31-1.91-2.19-3.34-2.19s-2.67.88-3.33 2.19c-1.4-.46-2.91-.2-3.92.81s-1.26 2.52-.8 3.91c-1.31.67-2.2 1.91-2.2 3.34s.89 2.67 2.2 3.34c-.46 1.39-.21 2.9.8 3.91s2.52 1.26 3.91.81c.67 1.31 1.91 2.19 3.34 2.19s2.68-.88 3.34-2.19c1.39.45 2.9.2 3.91-.81s1.27-2.52.81-3.91c1.31-.67 2.19-1.91 2.19-3.34zm-11.71 4.2L6.8 12.46l1.41-1.42 2.26 2.26 4.8-5.23 1.47 1.36-6.2 6.77z"></path></g>
+`
+
+// STOLEN FROM https://stackoverflow.com/questions/70507318/how-to-get-react-element-props-from-html-element-with-javascript
+function getReactProps(parent, target) {
+    // INITIAL VERY WIP FIREFOX HANDLING
+    parent = (window.chrome) ? parent : parent.wrappedJSObject;
+    target = (window.chrome) ? target : target.wrappedJSObject;
+
+    const keyof_ReactProps = Object.keys(parent).find(k => k.startsWith("__reactProps$"));
+    const symof_ReactFragment = Symbol.for("react.fragment");
+
+    //Find the path from target to parent
+    let path = [];
+    let elem = target;
+    while (elem !== parent) {
+        let index = 0;
+        for (let sibling = elem; sibling != null;) {
+            if (sibling[keyof_ReactProps]) index++;
+            sibling = sibling.previousElementSibling;
+        }
+        path.push({ child: elem, index });
+        elem = elem.parentElement;
+    }
+    //Walk down the path to find the react state props
+    let state = elem[keyof_ReactProps];
+    for (let i = path.length - 1; i >= 0 && state != null; i--) {
+        //Find the target child state index
+        let childStateIndex = 0, childElemIndex = 0;
+        while (childStateIndex < state.children.length) {
+            let childState = state.children[childStateIndex];
+            if (childState instanceof Object) {
+                //Fragment children are inlined in the parent DOM element
+                let isFragment = childState.type === symof_ReactFragment && childState.props.children.length;
+                childElemIndex += isFragment ? childState.props.children.length : 1;
+                if (childElemIndex === path[i].index) break;
+            }
+            childStateIndex++;
+        }
+        let childState = state.children[childStateIndex] ?? (childStateIndex === 0 ? state.children : null);
+        state = childState?.props;
+        elem = path[i].child;
+    }
+    return state;
+}
+
+
+function updateBlueTick(elem, props) {
+  if (props.isBlueVerified) {
+    elem.setAttribute('viewBox', '0 0 50 49')
+    elem.innerHTML = clown
+  } else {
+    elem.setAttribute('viewBox', '0 0 24 24')
+    elem.innerHTML = regulartick
+  }
+}
+
+function bluetickHandling(bluetick) {
+  let propsElem = getReactProps(bluetick.parentElement, bluetick)
+
+  if (propsElem.children !== undefined) {
+    const props = propsElem.children[0][0].props
+    if (props.isBlueVerified !== undefined) {
+      updateBlueTick(bluetick, props)
+    } else {
+      // VERY HACKY FIX DO BETTER NEXT TIME
+      const otherProps = propsElem.children[0][propsElem.children[0].length - 1].props
+      updateBlueTick(bluetick, otherProps)
+    }
+  } else {
+    const propsElemParent = getReactProps(bluetick.parentElement.parentElement.parentElement, bluetick.parentElement.parentElement)
+    const propsParent = propsElemParent.children[0][0].props
+    updateBlueTick(bluetick, propsParent)
+  }
+}
+
+function handleMutation(mutations) {
+  try {
+    for (let mutation of mutations) {
+      for (let elem of mutation.addedNodes) {
+        // SVG PATH DETECTION
+        // Thanks GH @artesea - https://gist.github.com/busybox11/53c76f57a577a47a19fab649a76f18e3?permalink_comment_id=4366043#gistcomment-4366043
+        // Author of this snippet
+        const blueticksPath = elem.querySelectorAll(regularVerifiedPath)
+        try {
+          for (let bluetick of blueticksPath) {
+            if (bluetick !== null) {
+              bluetickHandling(bluetick.parentElement.parentElement)
+            }
+          }
+        } catch(e) {sc.log(e)}
+
+        // GENERAL TWEETS WIP
+        // Class-based implementation
+        // Temporary, now deprecated in favor of SVG Path detection
+        /* const blueticksClass = elem.querySelectorAll('.r-13v1u17.r-4qtqp9.r-yyyyoo.r-1xvli5t')
+        try {
+          for (let bluetick of blueticksClass) {
+            if (bluetick !== null) {
+              bluetickHandling(bluetick)
+            }
+          }
+        } catch(e) {sc.log(e)} */
+
+        // PROFILE POPUPS
+        const profileBlueticks = elem.querySelectorAll('.css-1dbjc4n.r-xoduu5.r-1pcd2l5')
+        try {
+          for (let profileBluetick of profileBlueticks) {
+            if (profileBluetick !== null) {
+              if (profileBluetick.lastChild.firstChild.innerText.includes('Twitter Blue')) {
+                updateBlueTick(profileBluetick.firstChild, {isBlueVerified: true, isVerified: false})
+              }
+            }
+          }
+        } catch(e) {sc.log(e)}
+
+        // ENGLISH SPECIFIC
+        /* const blueticksEng = elem.querySelectorAll('[aria-label="Verified account"]')
+        try {
+          for (let bluetick of blueticksEng) {
+            if (bluetick !== null) {
+              bluetickHandling(bluetick)
+            }
+          }
+        } catch(e) {sc.log(e)} */
+      }
+    }
+  } catch(e) {}
+}
+
+const sc = {
+  log: (msg) => {
+    console.log('[clown]', msg)
+  }
+}
+
+const observer = new MutationObserver(handleMutation)
+observer.observe(document, { childList: true, subtree: true })
